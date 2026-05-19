9cab200c8e34c968b120780f81d0c40dde945605
subhi2013 <Jeyabalatharun@gmail.com>
2026-05-19 11:23:33 +0530

    beginner friendly simple quiz game

---
+++ simple quiz game.py
@@ -0,0 +1,85 @@
+print("welcome to the fun of quiz, lets get started")
+print("----INSTRUCTIONS----")
+print("there are 10 fun quizzes and it will give the answers instantly")
+print("if your score is 5 or above you passed or you will fail if it is less")
+score = 0
+print("-----/question starts/------")
+print("(1) what is the only country is also an entire continent?")
+a= str(input("enter the anwer:"))
+if a == "australia":
+ print("correct answer")
+ score = score + 1
+else:
+    print("wrong answer")
+print("(2) which organ in the human body consumes the most energy?")
+a = str(input("enter the answer:"))
+if a == "brain":
+    print("correct answer")
+    score = score + 1
+else:
+    print("wrong answer")
+print("(3) what is the rarest naturally occuring blood type in the world?")
+a = str(input("enter the answer:"))
+if a == "ab negative":
+    print("correct answer")
+    score = score + 1
+else:
+    print("wrong answer")
+print("(4) which artist painted the famous artwork the starry night?")
+a = str(input("enter the answer:"))
+if a == "vincent van gogh":
+    print("correct answer")
+    score = score + 1
+else:
+    print("wrong anser")
+print("(5) which is the worlds largest ocean?")
+a = str(input("enter the answer:"))
+if a == "pacific ocean":
+    print("correct answer")
+    score = score + 1
+else:
+    print("wrong answer")
+print("(6) which country has the most natural lakes in the world?")
+a = str(input("enter the answer:"))
+if a == "canada":
+    print("correct answer")
+    score = score + 1
+else:
+    print("wrong answer")
+print("(7) what is the hottest planet in our solar system?")
+a = str(input("enter the answer:"))
+if a == "venus":
+    print("correct answer")
+    score = score + 1
+else:
+    print("wrong answer")
+print("(8) which company created the popular video game minecraft?")
+a = str(input("enter the answer:"))
+if a == "mojang":
+    print("correct answer")
+    score = score + 1
+else:
+    print("wrong answer")
+print("(9) what is the only mammal capable of true,sustained fight?")
+a = str(input("enter the answer:"))
+if a == "bat":
+    print("correct answer:")
+    score = score + 1
+else:
+    print("wrong answer")
+print("(10) in which year did the titanic sink?")
+a = str(input("enter the answer:"))
+if a == "1912":
+    print("correct answer")
+    score = score + 1
+else:
+    print("wrong answer")
+if score >=5:
+    print("congragulations you passed")
+else:
+    print("sorry you failed better luck next time")
+
+
+
+ 
+          
