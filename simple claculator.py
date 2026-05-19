fb34ef35623ff5c3120eab789a525100783d5ed4
subhi2013 <Jeyabalatharun@gmail.com>
2026-05-19 09:39:46 +0530

    added simple calculator project

---
+++ simple claculator.py
@@ -0,0 +1,11 @@
+a = float(input("enter your first number:"))
+b = float(input("enter your second number:"))
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+print("-----CALCULATOR RESULTS-----")
+print("addition:" , c)
+print("subtraction:" , d)
+print("multiplucation:" , e)
+print("division:" , f)
