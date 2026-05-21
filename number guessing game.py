22cb0bd19d5bc8d24d15b328cfee5c142c16f53f
subhi2013 <Jeyabalatharun@gmail.com>
2026-05-21 09:41:02 +0530

    added simple number guessing game

---
+++ number guessing game.py
@@ -0,0 +1,10 @@
+number = 11
+while True:
+    a = int(input("guess the number:"))
+    if a == 11:
+        print("congrats you have found the right number")
+        break
+    else:
+        print("sorry you have found the wrong number try again")
+     
+    
